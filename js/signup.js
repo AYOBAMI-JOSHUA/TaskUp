0263397713a96946680b24a9a8f630bdc9b0a8b8
AYOBAMI-JOSHUA <ayobamij89@gmail.com>
2024-11-30 10:42:52 +0100

    first commit

---
+++ js/signup.js
@@ -0,0 +1,12 @@
+let signupBtn = document.getElementById("signupBtn")
+let signinBtn = document.getElementById("signinBtn")
+let nameField = document.getElementById("nameField")
+let title = document.getElementById("title")
+
+
+signupBtn.onclick = function(){
+    nameField.style.maxHeight = "0";
+    title.innerHTML = "Sign In";
+    signupBtn.classList.add("disable");
+    signinBtn.classList.remove("disable");
+}
